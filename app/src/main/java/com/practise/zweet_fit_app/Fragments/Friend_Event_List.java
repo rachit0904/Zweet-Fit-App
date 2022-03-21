8c46d54ef81c0a22e17853fdc89e7df294d984ea
Palash Rambhia <palashrambhia2@gmail.com>
2022-03-22 00:43:09 +0530

    Added Some UI and Friend Search Rv done

---
+++ app/src/main/java/com/practise/zweet_fit_app/Fragments/Friend_Event_List.java
@@ -0,0 +1,13 @@
+package com.practise.zweet_fit_app.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Friend_Event_List extends Fragment {
+
+}
